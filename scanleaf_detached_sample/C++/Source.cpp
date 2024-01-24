4dcf245e065f6f6caac1020ac6e156b74093994b
FubingZhang <fjfzzzffbb@gmail.com>
2024-01-24 21:44:22 +0800

    modified:   README.md 	modified:   scanleaf_detached_sample/C++/Source.cpp

--- scanleaf_detached_sample/C++/Source.cpp
+++ scanleaf_detached_sample/C++/Source.cpp
@@ -12,17 +12,17 @@ int main()
 	else {
 		cout << "ok" << endl;
 	}
-	cout << A.leaf_length << endl;//��
-	cout << A.leaf_width << endl;//��
-	cout << A.length_widith_ratio << endl;//������
-	cout << A.leaf_perimeter << endl;//�ܳ�
-	cout << A.leaf_area << endl;//���׾������
-	cout << A.rectangularity << endl;//���ζȣ�ҶƬ�����MER�����ֵ��
-	cout << A.densification << endl;//���ܶȣ��ܳ�ƽ���������ֵ��
-	cout << A.circularity << endl;//Բ���ԣ����ĵ��߽���ƽ������/���������
-	cout << A.sphericity << endl;//��״�ԣ�2DΪ�ڽ�Բ�뾶/���Բ�뾶��
-	cout << A.boundary_energy << endl;//�߽�����
-	cout << A.leaf_color << endl;//��ɫ
+	cout << A.leaf_length << endl;//长
+	cout << A.leaf_width << endl;//宽
+	cout << A.length_widith_ratio << endl;//长宽比
+	cout << A.leaf_perimeter << endl;//周长
+	cout << A.leaf_area << endl;//面积
+	cout << A.rectangularity << endl;//矩形度
+	cout << A.densification << endl;//致密度
+	cout << A.circularity << endl;//圆形度
+	cout << A.sphericity << endl;//球度
+	cout << A.boundary_energy << endl;//边界能量
+	cout << A.leaf_color << endl;//颜色
 	while (true) {}
 	return 0;
 }
