0cfc9188e392a2882361f323fdb00a61a83b87ca
FubinZhang <fjfzzzffbb@gmail.com>
2024-05-04 20:54:42 +0800

    2024.05.04

--- scanleaf_detached_sample/C++2Python/main.py
+++ scanleaf_detached_sample/C++2Python/main.py
@@ -10,6 +10,15 @@ result = scanner.Process_src(src)
 # 输出处理结果
 print("Leaf color:", result.leaf_color)
 print("Leaf length:", result.leaf_length)
+print("Leaf width:", result.leaf_width)
+print("Leaf length_widith_ratio:", result.length_widith_ratio)
+print("Leaf perimeter:", result.leaf_perimeter)
+print("Leaf area:", result.leaf_area)
+print("Leaf rectangularity:", result.rectangularity)
+print("Leaf densification:", result.densification)
+print("Leaf circularity:", result.circularity)
+print("Leaf sphericity:", result.sphericity)
+print("Leaf boundary_energy:", result.boundary_energy)
 # 其他属性以此类推
 
 # 如果有错误信息
